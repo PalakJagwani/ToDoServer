5aa8c6fe58bdf5261172d06de731905e443b62a4
Palak Jagwani <jagwanipalak02@gmail.com>
2024-06-08 15:20:11 +0530

    Update

--- index.js
+++ index.js
@@ -10,17 +10,11 @@ const app = express()
 dotenv.config({path : './.env'})
 
 app.use(cors({
-    origin : 'http://localhost:5174',
+    origin : 'http://localhost:5173',
     methods : 'GET, PUT, POST, DELETE',
     credentials : true
 }))
 
-app.options('*', (req, res) => {
-    res.set('Access-Control-Allow-Origin', '*');
-    res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.status(200).send();
-});
 
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json({extended:true}))
