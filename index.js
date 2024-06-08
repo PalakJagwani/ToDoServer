fd42a8fb283685ebb0a0d761c6199689d48de18c
Palak Jagwani <jagwanipalak02@gmail.com>
2024-06-08 14:51:51 +0530

    update cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express()
 dotenv.config({path : './.env'})
 
 app.use(cors({
-    origin : 'http://localhost:5174/',
+    origin : 'http://localhost:5174',
     methods : 'GET, PUT, POST, DELETE',
     credentials : true
 }))
